1d63dbd6f44dea9e6f17aca150c1fce913ba4d24
VikyChoi <vikychoi@gmail.com>
2019-03-07 16:26:40 +0800

    Text field added

--- src/main/java/lab3/ex2/Lab3Controller.java
+++ src/main/java/lab3/ex2/Lab3Controller.java
@@ -4,20 +4,30 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 public class Lab3Controller {
-
-    @FXML
-    private Label label1;
 
     @FXML
     private Button button1;
 
     @FXML
-    void buttonPressed(ActionEvent event) {
-    	label1.setText("Harder daddy!");
-    }
+    private Label label1;
+
+    @FXML
+    private TextField textField;
+    
     
 
+    @FXML
+    void KeyInput(KeyEvent event) {
+    	event.getCharacter();
+    }
+
+    @FXML
+    void buttonPressed(ActionEvent event) {
+    	label1.setText(textField.getText());
+    }
 
 }

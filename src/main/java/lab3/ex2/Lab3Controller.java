778c4136eae33412fc9cc41f7e11607d0cc2cc22
VikyChoi <vikychoi@gmail.com>
2019-03-07 14:35:41 +0800

    This is the first commit

---
+++ src/main/java/lab3/ex2/Lab3Controller.java
@@ -0,0 +1,23 @@
+package lab3.ex2;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Lab3Controller {
+
+    @FXML
+    private Label label1;
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    void buttonPressed(ActionEvent event) {
+    	label1.setText("Harder daddy!");
+    }
+    
+
+
+}
